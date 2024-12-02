92e4f0452ed509ecdd4a3867917424bf0f151c32
hsmazur <hugomazur16@gmail.com>
2024-12-02 08:51:39 -0300

    Update Bicicleta.js

--- CRUD_Bicicleta/Bicicleta.js
+++ CRUD_Bicicleta/Bicicleta.js
@@ -1,1 +1,11 @@
-
+class Bicicleta{
+    constructor (id,nome,fabricante,dataDeLancamento,preco,peso) {
+           this.id = id;
+           this.nome = nome;
+           this.fabricante = fabricante;
+           this.dataDeLancamento = dataDeLancamento;
+           this.preco = preco;
+           this.peso = peso;
+           this.posicaoNaLista = null;
+    }
+}
